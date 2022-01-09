8409a18aeca95429271bf3869751f020f752323d
lapinRagnar <lapinragnar@gmail.com>
2022-01-09 11:52:22 +0100

    notion de base asunchronous

---
+++ 1_node_course/weather-app/app.js
@@ -0,0 +1,11 @@
+console.log('starting');
+
+setTimeout(() => {
+
+    // cette message est afficher 2s apres 'stopping'
+    console.log("2 secondes timer");
+
+}, 2000)
+
+
+console.log('stopping');
