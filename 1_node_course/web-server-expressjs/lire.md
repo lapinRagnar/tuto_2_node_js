2abf3a560f426a52287e775399d730cdf3556752
lapinRagnar <lapinragnar@gmail.com>
2022-01-15 22:37:23 +0100

    express js - Serving up Static Assets

--- 1_node_course/web-server-expressjs/lire.md
+++ 1_node_course/web-server-expressjs/lire.md
@@ -5,6 +5,18 @@
 
 - créer un dossier src
 - creer src/app.js
-    
+
+#####
+#   Serving up Static Assets 
+#####
+
+- créer un dossier public
+- créer un fichier public/index.html
+- créer un fichier public/about.html
+- créer un fichier public/help.html
+
+- dans src/app.js
+    const path = require('path');
+    const publicDirectoryPath = path.join(__dirname, '../public')
 
 
--- 1_node_course/web-server-expressjs/src/app.js
+++ 1_node_course/web-server-expressjs/src/app.js
@@ -1,28 +1,16 @@
-const express = require('express')
+const path = require('path');
+const express = require('express');
 
 const app = express()
 const port = 8080
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-// le routing
-// render un html
-app.get('', (req, res) => {
-    res.send(` 
-        <h1> Bienvenue sur mon super site </h1>
-    `)
-})
+// permet d'afficher la page public/index.html - donc plus besoin de mettre la route app.get('', (req, res) => { res.send(`.... `) })
+// on peut supprimer les routes
+// et pour acceder a la page on fait, localhost ou localhost/about.html ou localhost/help.html
+app.use(express.static(publicDirectoryPath))
 
-// render un json
-app.get('/help', (req, res) => {
-    res.send({
-        titre: "mon super titre",
-        nom: "mon super nom",
-        prenom: "mon super prénom"
-    })
-})
 
-app.get('/about', (req, res) => {
-    res.send("Page a propos")
-})
 
 app.get('/weather-app', (req, res) => {
     res.send("Page météo")
