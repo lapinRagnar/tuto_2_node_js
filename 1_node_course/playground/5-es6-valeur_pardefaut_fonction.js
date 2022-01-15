f4abc634c982b3d0c06f46b51d7f96b991fdc702
lapinRagnar <lapinragnar@gmail.com>
2022-01-15 18:27:34 +0100

    comment faire un default function parameters

---
+++ 1_node_course/playground/5-es6-valeur_pardefaut_fonction.js
@@ -0,0 +1,8 @@
+// comment mettre une valeur par defaut dans un parametre - fonction
+const saluer = (nom = "utilisateur") => {
+    console.log("bonjour " + nom);
+}
+
+saluer("anarana iray")
+saluer()
+
